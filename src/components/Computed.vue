4255bcd8f0e2722dc44f17e76d492d4e4e0c6906
EH-SrdjanDjakovic <srdjan.djakovic@outlook.com>
2022-08-20 12:31:10 +0200

    1 file modified, 1 added, 1 deleted

---
+++ src/components/Computed.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { reactive, computed, ref } from 'vue';
+
+const author = reactive({
+  name: 'John Doe',
+  books: [
+    'Vue 2 - Advanced Guide',
+    'Vue 3 - Basic Guide',
+    'Vue 4 - The Mystery',
+  ],
+});
+
+// a computed ref
+const publishedBooksMessage = computed(() => {
+  return author.books.length > 0 ? 'Yes' : 'No';
+});
+/*********************************************************************/
+const firstName = ref('John');
+const lastName = ref('Doe');
+
+const fullName = computed({
+  // getter
+  get() {
+    return firstName.value + ' ' + lastName.value;
+  },
+  // setter
+  set(newValue) {
+    // Note: we are using destructuring assignment syntax here.
+    [firstName.value, lastName.value] = newValue.split(' ');
+  },
+});
+fullName.value = 'Vladimir Putin';
+</script>
+
+<template>
+  <p>Has published books:</p>
+  <span>{{ publishedBooksMessage }}</span>
+  <hr />
+  <p>{{ fullName }}</p>
+  <span>{{ firstName }}</span>
+</template>
